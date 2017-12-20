db1ae92f0bd3959bc6fecac5c8515da5f2176e23
Prem Kagrani <pkagrani@gmail.com>
2017-12-20 19:45:34 +0530

    bfs

---
+++ ELEVTRBL.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool visited[1000002];
+int press[1000002];
+int f,s,g,u,d;
+void bfs()
+{
+	queue<int> q;
+	q.push(s);
+	press[s]=0;
+	visited[s]=true;
+	while(!q.empty())
+	{
+		int val=q.front();
+		q.pop();
+		if(visited[g])
+			return;
+		if(val+u<=f && !visited[val+u])
+		{
+			q.push(val+u);
+			press[val+u]=press[val]+1;
+			visited[val+u]=true;
+		}
+		if(val-d>0 && !visited[val-d])
+		{
+			q.push(val-d);
+			press[val-d]=press[val]+1;
+			visited[val-d]=true;
+		}
+	}
+}
+int main() {
+	memset(press,0,sizeof(press));
+	memset(visited,false,sizeof(visited));
+	cin>>f>>s>>g>>u>>d;
+	bfs();
+	if(visited[g])
+		cout<<press[g];
+	else
+		cout<<"use the stairs";
+	return 0;
+}
